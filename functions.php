f4e780237638b9feb9bd6b3d0c30c7afc2b654b6
Travis Clark <travis@traviseugeneclark.com>
2018-11-06 11:54:10 -0800

    testing the modified rewrite_rule

--- functions.php
+++ functions.php
@@ -28,6 +28,7 @@ add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
 add_action('init', function() {
 	
   add_rewrite_rule('^listings/([a-zA-Z0-9-_]+)$', 'index.php?pagename=listing&sl=$matches[1]', 'top');
+  add_rewrite_rule('^listings/[a-zA-Z0-9-_]+/([a-zA-Z0-9-_]+)$', 'index.php?pagename=listing&sl=$matches[1]', 'top');
 
 });
 
