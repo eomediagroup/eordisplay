407b2ea2460823c1dc4fc68c91a74ce21443e94b
Travis Clark <travis@traviseugeneclark.com>
2018-12-18 14:12:25 -0800

    updates to overview metadata on listings

--- listing/metadata-default.php
+++ listing/metadata-default.php
@@ -14,69 +14,13 @@
 		<table role="grid" summary="Exterior details for [stratum_listing account=84 key='title']">
 			<tbody>
 			
-			<!-- Standard Overview -->	
+			<!-- Identity -->
+			
 			[stratum_listing_has_value account=84 key='seller']
 				<tr>
 					<td class="key">Seller</td>
 					<td class="value">[stratum_listing account=84 key='seller']</td>
 				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='acreage']
-				<tr>
-					<td class="key">Acres</td>
-					<td class="value">[stratum_listing account=84 key='acreage' transform="number(2)"]</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='year']
-				<tr>
-					<td class="key">Year Built</td>
-					<td class="value">[stratum_listing account=84 key='year']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='square-footage']
-				<tr>
-					<td class="key">Home Sqft</td>
-					<td class="value">[stratum_listing account=84 key='square-footage' transform="number(2)"]</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='bedrooms']
-				<tr>
-					<td class="key">Bedrooms</td>
-					<td class="value">[stratum_listing account=84 key='bedrooms']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='bathrooms']
-				<tr>
-					<td class="key">Bathrooms</td>
-					<td class="value">[stratum_listing account=84 key='bathrooms']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='price-sqft']
-				<tr>
-					<td class="key">Price/Sqft</td>
-					<td class="value">[stratum_listing account=84 key='price-sqft']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='mls']
-				<tr>
-					<td class="key">MLS#</td>
-					<td class="value">[stratum_listing account=84 key='mls']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			[stratum_listing_has_value account=84 key='region']
-				<tr>
-					<td class="key">County</td>
-					<td class="value">[stratum_listing account=84 key='region']</td>
-				</tr>
-			[/stratum_listing_has_value]
-			
-			[stratum_listing_has_value account=84 key='location-name']
-				[stratum_listing_multi account=84 key='location-name']
-				<tr>
-					<td class="key">Location</td>
-					<td class="value">{{location-name}}</td>
-				</tr>
-				[/stratum_listing_multi]
 			[/stratum_listing_has_value]
 			
 			[stratum_listing_has_value account=84 key='real-estate-agency']
@@ -93,24 +37,151 @@
 				</tr>
 				[/stratum_listing_multi]
 			[/stratum_listing_has_value]
+			
+			<!-- Identifiers -->
+			
+			[stratum_listing_has_value account=84 key='price']
+				<tr>
+					<td class="key">Price</td>
+					<td class="value">[stratum_listing account=84 key='price' transform="money"]</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='year']
+				<tr>
+					<td class="key">Year</td>
+					<td class="value">[stratum_listing account=84 key='year']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='condition']
+				<tr>
+					<td class="key">Condition</td>
+					<td class="value">[stratum_listing account=84 key='condition']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='manufacturer']
+				<tr>
+					<td class="key">Manufacturer</td>
+					<td class="value">[stratum_listing account=84 key='manufacturer']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='model']
+				<tr>
+					<td class="key">Model</td>
+					<td class="value">[stratum_listing account=84 key='model']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='vin']
+				<tr>
+					<td class="key">VIN</td>
+					<td class="value">[stratum_listing account=84 key='vin']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='stock-number']
+				<tr>
+					<td class="key">Stock Number</td>
+					<td class="value">[stratum_listing account=84 key='stock-number']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='serial-number']
+				<tr>
+					<td class="key">Serial Number</td>
+					<td class="value">[stratum_listing account=84 key='serial-number']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='engine']
+				<tr>
+					<td class="key">Engine</td>
+					<td class="value">[stratum_listing account=84 key='engine']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='color']
+				<tr>
+					<td class="key">Color</td>
+					<td class="value">[stratum_listing account=84 key='color']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			<!-- Real Estate -->
+			
+			[stratum_listing_has_value account=84 key='acreage']
+				<tr>
+					<td class="key">Acres</td>
+					<td class="value">[stratum_listing account=84 key='acreage' transform="number(2)"]</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='square-footage']
+				<tr>
+					<td class="key">Home Sqft</td>
+					<td class="value">[stratum_listing account=84 key='square-footage' transform="number(2)"]</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='bedrooms']
+				<tr>
+					<td class="key">Bedrooms</td>
+					<td class="value">[stratum_listing account=84 key='bedrooms']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='bathrooms']
+				<tr>
+					<td class="key">Bathrooms</td>
+					<td class="value">[stratum_listing account=84 key='bathrooms']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='price-sqft']
+				<tr>
+					<td class="key">Price/Sqft</td>
+					<td class="value">[stratum_listing account=84 key='price-sqft']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='mls']
+				<tr>
+					<td class="key">MLS#</td>
+					<td class="value">[stratum_listing account=84 key='mls']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
+			[stratum_listing_has_value account=84 key='region']
+				<tr>
+					<td class="key">County</td>
+					<td class="value">[stratum_listing account=84 key='region']</td>
+				</tr>
+			[/stratum_listing_has_value]
+			
 			[stratum_listing_has_value account=84 key='lender-down-payment']
 				<tr>
 					<td class="key">Down Payment</td>
 					<td class="value">[stratum_listing account=84 key='lender-down-payment']%</td>
 				</tr>
 			[/stratum_listing_has_value]
+			
 			[stratum_listing_has_value account=84 key='lender-max-ltv']
 				<tr>
 					<td class="key">Max LTV</td>
 					<td class="value">[stratum_listing account=84 key='lender-max-ltv']%</td>
 				</tr>
 			[/stratum_listing_has_value]
+			
 			[stratum_listing_has_value account=84 key='lender-min-fico']
 				<tr>
 					<td class="key">Min FICO</td>
 					<td class="value">[stratum_listing account=84 key='lender-min-fico']</td>
 				</tr>
 			[/stratum_listing_has_value]
+			
 			[stratum_listing_has_value account=84 key='lender-max-dti']
 				<tr>
 					<td class="key">Max DTI</td>
@@ -125,11 +196,15 @@
 				</tr>
 			[/stratum_listing_has_value]
 			
-			[stratum_listing_has_value account=84 key='rentals-lease']
+			<!-- Location -->
+			
+			[stratum_listing_has_value account=84 key='location-name']
+				[stratum_listing_multi account=84 key='location-name']
 				<tr>
-					<td class="key">Lease</td>
-					<td class="value">[stratum_listing account=84 key='rentals-lease']</td>
+					<td class="key">Location</td>
+					<td class="value">{{location-name}}</td>
 				</tr>
+				[/stratum_listing_multi]
 			[/stratum_listing_has_value]
 			
 			</tbody>
